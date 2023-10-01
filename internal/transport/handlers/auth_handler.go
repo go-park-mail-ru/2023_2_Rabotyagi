921c8e92a782fd7301e0ee098319cfb6a7344c03
Vilinvil <ivn15072002@gmail.com>
2023-10-01 20:57:51 +0300

    [1:2] api VilinVil  changed responses and endpoint signup

--- internal/transport/handlers/auth_handler.go
+++ internal/transport/handlers/auth_handler.go
@@ -3,13 +3,15 @@ package handler
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 
 	auth "github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/authorization"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/storage"
 )
+
+// TODO from config
+var secret = []byte("super-secret")
 
 type AuthHandler struct {
 	storage *storage.AuthStorageMap
@@ -23,91 +25,101 @@ func (h *AuthHandler) InitRoutes() http.Handler {
 		storage: storageMap,
 	}
 
-	router.HandleFunc("/api/v1/signin/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Вы на логине")
-		log.Println(r.URL.Path)
-
-		w.Header().Set("Content-Type", "application/json")
-
-		if r.Method == http.MethodPost {
-			authHandler.signInHandler(w, r)
-		} else {
-			http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
-		}
-	})
-
-	router.HandleFunc("/api/v1/signup/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.URL.Path)
-		
-		w.Header().Set("Content-Type", "application/json")
-
-		if r.Method == http.MethodPost {
-			authHandler.signUpHandler(w, r)
-		} else {
-			http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
-		}
-	})
+	router.HandleFunc("/api/v1/signup/", authHandler.signUpHandler)
 
 	return router
 }
 
-var errUserExists = errors.New("the user already exists")
+func sendErr(w http.ResponseWriter, errResponse ErrorResponse) {
+	response, err := json.Marshal(errResponse)
+	if err != nil {
+		log.Printf("%v\n", err)
+		http.Error(w, ErrInternalServer.Body.Error, http.StatusInternalServerError)
+	}
 
-var secret = []byte("super-secret")
+	_, err = w.Write(response)
+	if err != nil {
+		log.Printf("%v\n", err)
+		http.Error(w, ErrInternalServer.Body.Error, http.StatusInternalServerError)
+	}
+}
+
+func sendResponse(w http.ResponseWriter, response Response) {
+	responseSend, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("%v\n", err)
+		http.Error(w, ErrInternalServer.Body.Error, http.StatusInternalServerError)
+	}
+
+	_, err = w.Write(responseSend)
+	if err != nil {
+		log.Printf("%v\n", err)
+		http.Error(w, ErrInternalServer.Body.Error, http.StatusInternalServerError)
+	}
+}
 
 func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
+	if r.Method != http.MethodPost {
+		http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
+	}
+
 	decoder := json.NewDecoder(r.Body)
 
-	newUser := new(storage.PreUser)
-	err := decoder.Decode(newUser)
+	preUser := new(storage.PreUser)
+	if err := decoder.Decode(preUser); err != nil {
+		log.Printf("%v\n", err)
+		sendErr(w, ErrBadRequest)
+
+		return
+	}
+
+	if h.storage.IsUserExist(preUser.Email) {
+		log.Printf("already exist user %v\n", preUser)
+		sendErr(w, ErrUserAlreadyExist)
+
+		return
+	}
+
+	err := h.storage.CreateUser(preUser)
 	if err != nil {
-		log.Printf("error while unmarshalling JSON: %s", err)
-		w.Write([]byte("{}"))
+		log.Printf("%v", err)
+		sendErr(w, ErrInternalServer)
+
 		return
 	}
 
-	fmt.Println(newUser)
-
-	// уже есть юзер с таким именем
-	if h.storage.IsUserExist(newUser.Email) {
-		log.Printf("%s", errUserExists)
-		w.Write([]byte("{}"))
-		return
-	}
-	
-	// создаем юзера
-	h.storage.CreateUser(newUser)
-
-	userWithId, err := h.storage.GetUser(newUser.Email)
+	user, err := h.storage.GetUser(preUser.Email)
 	if err != nil {
-		log.Printf("error while getting user: %s", err)
-		w.Write([]byte("{}"))
+		log.Printf("%v", err)
+		sendErr(w, ErrInternalServer)
+
 		return
 	}
 
-	// генерируем jwt токен
-	jwtStr, err := auth.GenerateJwtToken(userWithId, secret)
+	jwtStr, err := auth.GenerateJwtToken(user, secret)
 	if err != nil {
-		log.Printf("%s", err)
-		w.Write([]byte("{}"))
+		log.Printf("%v", err)
+		sendErr(w, ErrInternalServer)
+
 		return
 	}
-	
+
 	w.Header().Set("Content-Type", "application/json")
 
-	// выставляем куку
-	cookie := &http.Cookie{
+	cookie := &http.Cookie{ //nolint:exhaustruct,exhaustivestruct
 		Name:  "session_id",
 		Value: jwtStr,
 	}
 
 	http.SetCookie(w, cookie)
 
-	// Отправляем токен как строку в теле ответа
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(jwtStr))
+
+	sendResponse(w, ResponseSuccessfulSignUp)
+
+	log.Printf("added user: %v", user)
 }
 
 var (
@@ -118,7 +130,7 @@ func (h *AuthHandler) signInHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	// декодим json из реквеста в storage.PreUser
-	decoder := json.NewDecoder(r.Body) 
+	decoder := json.NewDecoder(r.Body)
 
 	user := new(storage.PreUser)
 	err := decoder.Decode(user)
@@ -150,9 +162,9 @@ func (h *AuthHandler) signInHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("{}"))
 		return
 	}
-	
+
 	w.Header().Set("Content-Type", "application/json")
-	
+
 	// выставляем куку
 	cookie := &http.Cookie{
 		Name:  "session_id",
