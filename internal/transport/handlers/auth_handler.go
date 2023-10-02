7222f67faea241ae6a8ded4fa04e3bd0ea5b381e
SanExpett <alexkrasnoperov09@gmail.com>
2023-10-02 21:03:03 +0300

    [1:5] merged with api SanExpett

--- internal/transport/handlers/auth_handler.go
+++ internal/transport/handlers/auth_handler.go
@@ -2,27 +2,16 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
+	"time"
 
-	auth "github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/authorization"
+	auth "github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/jwt"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/storage"
 )
 
-// TODO from config
-var secret = []byte("super-secret")
+const timeTokenLife = 24 * time.Hour
 
-// signUpHandler godoc
-//
-//  @Summary    register user
-//  @Description  register new user
-//  @Accept      json
-//  @Produce    json
-//  @Param      user  body    preUser  true  "User"
-//  @Success    200  {object}  myError
-//  @Failure    500  {object}  myError
-//  @Router      /signup/ [post]
 func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
@@ -35,14 +24,14 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	preUser := new(storage.PreUser)
 	if err := decoder.Decode(preUser); err != nil {
 		log.Printf("%v\n", err)
-		sendErr(w, ErrBadRequest)
+		sendResponse(w, ErrBadRequest)
 
 		return
 	}
 
 	if h.storage.IsUserExist(preUser.Email) {
 		log.Printf("already exist user %v\n", preUser)
-		sendErr(w, ErrUserAlreadyExist)
+		sendResponse(w, ErrUserAlreadyExist)
 
 		return
 	}
@@ -50,7 +39,7 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	err := h.storage.CreateUser(preUser)
 	if err != nil {
 		log.Printf("%v", err)
-		sendErr(w, ErrInternalServer)
+		sendResponse(w, ErrInternalServer)
 
 		return
 	}
@@ -58,15 +47,16 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	user, err := h.storage.GetUser(preUser.Email)
 	if err != nil {
 		log.Printf("%v", err)
-		sendErr(w, ErrInternalServer)
+		sendResponse(w, ErrInternalServer)
 
 		return
 	}
 
-	jwtStr, err := auth.GenerateJwtToken(user, secret)
+	jwtStr, err := auth.GenerateJwtToken(
+		&auth.UserJwtPayload{UserID: user.ID, Email: user.Email, Expire: time.Now().Add(timeTokenLife).Unix()})
 	if err != nil {
 		log.Printf("%v", err)
-		sendErr(w, ErrInternalServer)
+		sendResponse(w, ErrInternalServer)
 
 		return
 	}
@@ -74,7 +64,7 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	cookie := &http.Cookie{ //nolint:exhaustruct,exhaustivestruct
-		Name:  "session_id",
+		Name:  CookieAuthName,
 		Value: jwtStr,
 	}
 
@@ -87,72 +77,84 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("added user: %v", user)
 }
 
-var (
-	errBadCredentials = errors.New("email or password is incorrect")
-)
-
-// signInHandler godoc
-//
-//  @Summary    login user
-//  @Description  login user
-//  @Accept      json
-//  @Produce    json
-//  @Param      user  body    preUser  true  "User"
-//  @Success    200  {object}  myError
-//  @Failure    500  {object}  myError
-//  @Router      /signin/ [post]
 func (h *AuthHandler) signInHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-  
+
 	if r.Method != http.MethodPost {
-	  http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
 	}
-  
+
 	decoder := json.NewDecoder(r.Body)
-  
+
 	preUser := new(storage.PreUser)
-  
+
 	if err := decoder.Decode(preUser); err != nil {
-	  log.Printf("%v\n", err)
-	  sendErr(w, ErrBadRequest)
-  
-	  return
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrBadRequest)
+
+		return
 	}
-  
+
 	if !h.storage.IsUserExist(preUser.Email) {
-	  log.Printf("user is not exists %v\n", preUser)
-	  sendErr(w, ErrWrongCredentials)
-  
-	  return
+		log.Printf("user is not exists %v\n", preUser)
+		sendResponse(w, ErrWrongCredentials)
+
+		return
 	}
-  
+
 	user, err := h.storage.GetUser(preUser.Email)
 	if err != nil || preUser.Password != user.Password {
-	  log.Printf("%v\n", err)
-	  sendErr(w, ErrWrongCredentials)
-  
-	  return
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrWrongCredentials)
+
+		return
 	}
-  
-	jwtStr, err := auth.GenerateJwtToken(user, secret)
+
+	jwtStr, err := auth.GenerateJwtToken(
+		&auth.UserJwtPayload{UserID: user.ID, Email: user.Email, Expire: time.Now().Add(timeTokenLife).Unix()})
 	if err != nil {
-	  log.Printf("%v\n", err)
-	  sendErr(w, ErrInternalServer)
-  
-	  return
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrInternalServer)
+
+		return
 	}
-  
+
 	w.Header().Set("Content-Type", "application/json")
-  
+
 	cookie := &http.Cookie{ //nolint:exhaustruct,exhaustivestruct
-	  Name:  "session_id",
-	  Value: jwtStr,
+		Name:  CookieAuthName,
+		Value: jwtStr,
 	}
-  
+
 	http.SetCookie(w, cookie)
 	w.WriteHeader(http.StatusOK)
-  
+
 	sendResponse(w, ResponseSuccessfulSignIn)
-  
+
 	log.Printf("sign user: %v", user)
-  }
+}
+
+func (h *AuthHandler) logOut(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	if r.Method != http.MethodPost {
+		http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
+	}
+
+	cookie, err := r.Cookie(CookieAuthName)
+	if err != nil {
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrUnauthorized)
+
+		return
+	}
+
+	cookie.Expires = time.Now()
+
+	http.SetCookie(w, cookie)
+	w.WriteHeader(http.StatusOK)
+
+	sendResponse(w, ResponseSuccessfulLogOut)
+
+	log.Printf("logout user with cookie: %v", cookie)
+}
--- internal/transport/handlers/responses.go
+++ internal/transport/handlers/responses.go
@@ -39,24 +39,22 @@ type PostsListResponse struct {
 const (
 	StatusResponseSuccessful = 200
 	StatusErrBadRequest      = 400
+	StatusUnauthorized       = 401
 	StatusErrServerError     = 500
+
+	CookieAuthName = "session_id"
 )
 
 var (
-	ResponseSuccessfulSignUp = Response{
-		Status: StatusResponseSuccessful, 
-		Body: ResponseBody{Message: "Successful sign up"},
-	}
-
-	ResponseSuccessfulSignIn = Response{
-		Status: StatusResponseSuccessful, 
-		Body: ResponseBody{Message: "Successful sign in"},
-	}
+	ResponseSuccessfulSignUp = Response{Status: StatusResponseSuccessful, Body: ResponseBody{Message: "Successful sign up"}}
+	ResponseSuccessfulSignIn = Response{Status: StatusResponseSuccessful, Body: ResponseBody{Message: "Successful sign in"}}
+	ResponseSuccessfulLogOut = Response{Status: StatusResponseSuccessful, Body: ResponseBody{Message: "Successful log out"}}
   
 	ErrInternalServer   = ErrorResponse{Status: StatusErrServerError, Body: ResponseBodyError{Error: "Error in server"}}
 	ErrBadRequest       = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "Wrong request"}}
 	ErrUserAlreadyExist = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "User with same email already exist"}}
 	ErrWrongCredentials = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "Uncorrect login or password"}}
+	ErrUnauthorized     = ErrorResponse{Status: StatusUnauthorized, Body: ResponseBodyError{Error: "You unauthorized"}}
 
 	ErrPostNotExist = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "Post not exists"}}
 	ErrNoSuchCountOfPosts = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "n > posts count"}}
