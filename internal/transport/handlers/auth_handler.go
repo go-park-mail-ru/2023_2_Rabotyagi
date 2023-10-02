945cf3a20824d6b7e1d56657730cf798112431c2
Vilinvil <ivn15072002@gmail.com>
2023-10-02 15:35:01 +0300

    [1:2] api VilinVil  changed signin, added logout, upgraded jwt

--- internal/transport/handlers/auth_handler.go
+++ internal/transport/handlers/auth_handler.go
@@ -2,16 +2,15 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
+	"time"
 
-	auth "github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/authorization"
+	auth "github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/jwt"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/storage"
 )
 
-// TODO from config
-var secret = []byte("super-secret")
+const timeTokenLife = 24 * time.Hour
 
 type AuthHandler struct {
 	storage *storage.AuthStorageMap
@@ -26,25 +25,13 @@ func (h *AuthHandler) InitRoutes() http.Handler {
 	}
 
 	router.HandleFunc("/api/v1/signup/", authHandler.signUpHandler)
+	router.HandleFunc("/api/v1/signin/", authHandler.signInHandler)
+	router.HandleFunc("/api/v1/logout/", authHandler.logOut)
 
 	return router
 }
 
-func sendErr(w http.ResponseWriter, errResponse ErrorResponse) {
-	response, err := json.Marshal(errResponse)
-	if err != nil {
-		log.Printf("%v\n", err)
-		http.Error(w, ErrInternalServer.Body.Error, http.StatusInternalServerError)
-	}
-
-	_, err = w.Write(response)
-	if err != nil {
-		log.Printf("%v\n", err)
-		http.Error(w, ErrInternalServer.Body.Error, http.StatusInternalServerError)
-	}
-}
-
-func sendResponse(w http.ResponseWriter, response Response) {
+func sendResponse(w http.ResponseWriter, response any) {
 	responseSend, err := json.Marshal(response)
 	if err != nil {
 		log.Printf("%v\n", err)
@@ -70,14 +57,14 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	preUser := new(storage.PreUser)
 	if err := decoder.Decode(preUser); err != nil {
 		log.Printf("%v\n", err)
-		sendErr(w, ErrBadRequest)
+		sendResponse(w, ErrBadRequest)
 
 		return
 	}
 
 	if h.storage.IsUserExist(preUser.Email) {
 		log.Printf("already exist user %v\n", preUser)
-		sendErr(w, ErrUserAlreadyExist)
+		sendResponse(w, ErrUserAlreadyExist)
 
 		return
 	}
@@ -85,7 +72,7 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	err := h.storage.CreateUser(preUser)
 	if err != nil {
 		log.Printf("%v", err)
-		sendErr(w, ErrInternalServer)
+		sendResponse(w, ErrInternalServer)
 
 		return
 	}
@@ -93,15 +80,16 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	user, err := h.storage.GetUser(preUser.Email)
 	if err != nil {
 		log.Printf("%v", err)
-		sendErr(w, ErrInternalServer)
+		sendResponse(w, ErrInternalServer)
 
 		return
 	}
 
-	jwtStr, err := auth.GenerateJwtToken(user, secret)
+	jwtStr, err := auth.GenerateJwtToken(
+		&auth.UserJwtPayload{UserID: user.ID, Email: user.Email, Expire: time.Now().Add(timeTokenLife).Unix()})
 	if err != nil {
 		log.Printf("%v", err)
-		sendErr(w, ErrInternalServer)
+		sendResponse(w, ErrInternalServer)
 
 		return
 	}
@@ -109,7 +97,7 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	cookie := &http.Cookie{ //nolint:exhaustruct,exhaustivestruct
-		Name:  "session_id",
+		Name:  CookieAuthName,
 		Value: jwtStr,
 	}
 
@@ -122,58 +110,84 @@ func (h *AuthHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("added user: %v", user)
 }
 
-var (
-	errBadCredentials = errors.New("email or password is incorrect")
-)
-
 func (h *AuthHandler) signInHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	// декодим json из реквеста в storage.PreUser
+	if r.Method != http.MethodPost {
+		http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
+	}
+
 	decoder := json.NewDecoder(r.Body)
 
-	user := new(storage.PreUser)
-	err := decoder.Decode(user)
+	preUser := new(storage.PreUser)
+
+	if err := decoder.Decode(preUser); err != nil {
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrBadRequest)
+
+		return
+	}
+
+	if !h.storage.IsUserExist(preUser.Email) {
+		log.Printf("user is not exists %v\n", preUser)
+		sendResponse(w, ErrWrongCredentials)
+
+		return
+	}
+
+	user, err := h.storage.GetUser(preUser.Email)
+	if err != nil || preUser.Password != user.Password {
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrWrongCredentials)
+
+		return
+	}
+
+	jwtStr, err := auth.GenerateJwtToken(
+		&auth.UserJwtPayload{UserID: user.ID, Email: user.Email, Expire: time.Now().Add(timeTokenLife).Unix()})
 	if err != nil {
-		log.Printf("error while unmarshalling JSON: %s", err)
-		w.Write([]byte("{}"))
-		return
-	}
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrInternalServer)
 
-	// нет юзера с таким именем
-	if !h.storage.IsUserExist(user.Email) {
-		log.Printf("user is not exists")
-		w.Write([]byte("{}"))
-		return
-	}
-
-	// неправильный пароль
-	userWithId, err := h.storage.GetUser(user.Email)
-	if err != nil || user.Password != userWithId.Password {
-		log.Printf("error while getting user: %s", err)
-		w.Write([]byte("{}"))
-		return
-	}
-
-	// генерируем jwt токен из юзера
-	jwtStr, err := auth.GenerateJwtToken(userWithId, secret)
-	if err != nil {
-		log.Printf("%s", err)
-		w.Write([]byte("{}"))
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 
-	// выставляем куку
-	cookie := &http.Cookie{
-		Name:  "session_id",
+	cookie := &http.Cookie{ //nolint:exhaustruct,exhaustivestruct
+		Name:  CookieAuthName,
 		Value: jwtStr,
 	}
 
 	http.SetCookie(w, cookie)
-
-	// Отправляем токен как строку в теле ответа
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(jwtStr))
+
+	sendResponse(w, ResponseSuccessfulSignIn)
+
+	log.Printf("sign user: %v", user)
+}
+
+func (h *AuthHandler) logOut(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	if r.Method != http.MethodPost {
+		http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
+	}
+
+	cookie, err := r.Cookie(CookieAuthName)
+	if err != nil {
+		log.Printf("%v\n", err)
+		sendResponse(w, ErrUnauthorized)
+
+		return
+	}
+
+	cookie.Expires = time.Now()
+
+	http.SetCookie(w, cookie)
+	w.WriteHeader(http.StatusOK)
+
+	sendResponse(w, ResponseSuccessfulLogOut)
+
+	log.Printf("logout user with cookie: %v", cookie)
 }
--- internal/transport/handlers/responses.go
+++ internal/transport/handlers/responses.go
@@ -21,16 +21,20 @@ type Response struct {
 const (
 	StatusResponseSuccessful = 200
 	StatusErrBadRequest      = 400
+	StatusUnauthorized       = 401
 	StatusErrServerError     = 500
+
+	CookieAuthName = "session_id"
 )
 
 var (
-	ResponseSuccessfulSignUp = Response{
-		Status: StatusResponseSuccessful,
-		Body:   ResponseBody{Message: "Successful sign up"},
-	}
+	ResponseSuccessfulSignUp = Response{Status: StatusResponseSuccessful, Body: ResponseBody{Message: "Successful sign up"}}
+	ResponseSuccessfulSignIn = Response{Status: StatusResponseSuccessful, Body: ResponseBody{Message: "Successful sign in"}}
+	ResponseSuccessfulLogOut = Response{Status: StatusResponseSuccessful, Body: ResponseBody{Message: "Successful log out"}}
 
 	ErrInternalServer   = ErrorResponse{Status: StatusErrServerError, Body: ResponseBodyError{Error: "Error in server"}}
 	ErrBadRequest       = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "Wrong request"}}
 	ErrUserAlreadyExist = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "User with same email already exist"}}
+	ErrWrongCredentials = ErrorResponse{Status: StatusErrBadRequest, Body: ResponseBodyError{Error: "Uncorrect login or password"}}
+	ErrUnauthorized     = ErrorResponse{Status: StatusUnauthorized, Body: ResponseBodyError{Error: "You unauthorized"}}
 )
