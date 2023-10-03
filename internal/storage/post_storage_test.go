68ee74b28dee2873aaeb9b2b24308c96c126be4a
SanExpett <alexkrasnoperov09@gmail.com>
2023-10-03 14:50:34 +0300

    [1:3] add userSt error tests

--- internal/storage/post_storage_test.go
+++ internal/storage/post_storage_test.go
@@ -4,7 +4,6 @@ import (
     "testing"
 
 	"github.com/stretchr/testify/assert"
-    //"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/errors"
     "github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/storage"
 )
 
@@ -27,7 +26,7 @@ func TestPostStorageMapGet(t *testing.T) {
     assert.Equal(t, expectedID, storageMap.GetPostsCount())
     
 	postInMap, _ := storageMap.GetPost(uint64(1))
-    // Проверяем, что добавление существующего поста заменяет его значениями нового поста
+
     assert.Equal(t, uint64(1), postInMap.ID)
 	assert.Equal(t, prePost.AuthorID, postInMap.AuthorID)
 	assert.Equal(t, prePost.Description, postInMap.Description)
@@ -36,6 +35,25 @@ func TestPostStorageMapGet(t *testing.T) {
 	assert.Equal(t, prePost.SafeTransaction, postInMap.SafeTransaction)
 	assert.Equal(t, prePost.Delivery, postInMap.Delivery)
 	assert.Equal(t, prePost.City, postInMap.City)
+}
+
+func TestPostStorageMapGetError(t *testing.T) {
+    storageMap := storage.NewPostStorageMap()
+    prePost := &storage.PrePost{
+        AuthorID:        1,
+        Title:           "Test Post",
+        Description:     "This is a test post",
+        Price:           100,
+        SafeTransaction: true,
+        Delivery:        true,
+        City:            "Moscow",
+    }
+
+    storageMap.AddPost(prePost)
+    
+	_, err := storageMap.GetPost(2)
+
+    assert.NotNil(t, err)
 }
 
 func TestPostStorageMapGetList(t *testing.T) {
@@ -85,4 +103,23 @@ func TestPostStorageMapGetList(t *testing.T) {
 	assert.Equal(t, prePost2.SafeTransaction, postInMap2.SafeTransaction)
 	assert.Equal(t, prePost2.Delivery, postInMap2.Delivery)
 	assert.Equal(t, prePost2.City, postInMap2.City)
+}
+
+func TestPostStorageMapGetListError(t *testing.T) {
+    storageMap := storage.NewPostStorageMap()
+    prePost := &storage.PrePost {
+        AuthorID:        1,
+        Title:           "Test Post",
+        Description:     "This is a test post",
+        Price:           100,
+        SafeTransaction: true,
+        Delivery:        true,
+        City:            "Moscow",
+    }
+
+    storageMap.AddPost(prePost)
+    
+	_, err := storageMap.GetNPosts(10)
+
+    assert.NotNil(t, err)
 }
