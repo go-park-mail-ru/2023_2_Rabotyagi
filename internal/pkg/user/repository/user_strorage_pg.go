245e1a710e87166b6c14a8b7512981c9438a193f
SanExpett <alexkrasnoperov09@gmail.com>
2023-10-25 15:01:05 +0300

    [2-3] add IsUserExist storage func SanExpett

--- internal/pkg/user/repository/user_strorage_pg.go
+++ internal/pkg/user/repository/user_strorage_pg.go
@@ -2,15 +2,16 @@ package repository
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/models"
 	"github.com/jackc/pgx/v5"
+	"log"
 )
 
 const (
 	GetUserById = "SELECT id, email, phone, name, pass, birthday FROM public.\"user\" WHERE id=$1"
 	CreateUser  = "INSERT INTO public.\"user\" (email, phone, name, pass, birthday) VALUES ($1, $2, $3, $4, $5)"
-	IsUserExist = "\"SELECT id FROM public.\"user\" WHERE email=$1\""
+	IsUserExist = "SELECT id FORM public.\"user\" WHERE email=$1 AND phone=$2"
+	// UserNotExist = "SELECT id FORM public.\"user\" WHERE email=$1 AND phone=$2"
 )
 
 //var (
@@ -19,9 +20,10 @@ const (
 //)
 
 type IUserStorage interface {
-	GetUserById(userID uint64) (*models.User, error)
-	CreateUser(user *models.PreUser) error
-	IsUserExist(email string) bool
+	GetUserById(ctx context.Context, userID uint64) (*models.User, error)
+	CreateUser(ctx context.Context, user *models.PreUser) error
+	IsUserExist(ctx context.Context, email string) bool
+	// UserNotExist(ctx context.Context, email string, phone string) bool
 }
 
 type UserStorage struct {
@@ -42,10 +44,26 @@ func (u *UserStorage) GetUserById(ctx context.Context, id uint64) (*models.User,
 	}
 
 	if err := userLine.Scan(&user.ID, &user.Email, &user.Phone, &user.Name, &user.Pass, &user.Birthday); err != nil {
-		err = fmt.Errorf("error happened in row.Scan: %w", err)
+		log.Printf("%v\n", err)
 
 		return nil, err
 	}
 
 	return &user, nil
+}
+
+func (u *UserStorage) IsUserExist(ctx context.Context, email string, phone string) (bool, error) {
+	userIdRow := u.db.QueryRow(ctx, IsUserExist, email, phone)
+	var userId string
+
+	if err := userIdRow.Scan(userId); err != nil {
+		log.Printf("%v\n", err)
+
+		return false, err
+	}
+
+	if userId != "" {
+		return true, nil
+	}
+	return false, nil
 }
