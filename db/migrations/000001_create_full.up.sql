1fbb75616b0b4d5c5ae56a0d444d29362a6fcf6f
Vilinvil <ivn15072002@gmail.com>
2023-10-31 17:51:05 +0300

    [2-2]-db-VilinVil
    added names for constraints on length of fields

--- db/migrations/000001_create_full.up.sql
+++ db/migrations/000001_create_full.up.sql
@@ -8,17 +8,22 @@ CREATE SEQUENCE IF NOT EXISTS favourite_id_seq;
 CREATE TABLE IF NOT EXISTS public."user"
 (
     id       BIGINT DEFAULT NEXTVAL('user_id_seq'::regclass) NOT NULL PRIMARY KEY,
-    email    TEXT UNIQUE                                     NOT NULL CHECK (email <> '') CHECK (LENGTH(email) <= 256),
-    phone    TEXT UNIQUE                                     NOT NULL CHECK (phone <> '') CHECK (LENGTH(phone) <= 18),
-    name     TEXT                                            NOT NULL CHECK (name <> '') CHECK (LENGTH(name) <= 256),
-    password TEXT                                            NOT NULL CHECK (password <> '') CHECK (LENGTH(password) <= 256),
+    email    TEXT UNIQUE                                     NOT NULL CHECK (email <> '')
+        CONSTRAINT max_len_email CHECK (LENGTH(email) <= 256),
+    phone    TEXT UNIQUE                                     NOT NULL CHECK (phone <> '')
+        CONSTRAINT max_len_phone CHECK (LENGTH(phone) <= 18),
+    name     TEXT                                            NOT NULL CHECK (name <> '')
+        CONSTRAINT max_len_name CHECK (LENGTH(name) <= 256),
+    password TEXT                                            NOT NULL CHECK (password <> '')
+        CONSTRAINT max_len_password CHECK (LENGTH(password) <= 256),
     birthday TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE IF NOT EXISTS public."category"
 (
     id        BIGINT DEFAULT NEXTVAL('category_id_seq'::regclass) NOT NULL PRIMARY KEY,
-    name      TEXT UNIQUE                                         NOT NULL CHECK (name <> '') CHECK (LENGTH(name) <= 256),
+    name      TEXT UNIQUE                                         NOT NULL CHECK (name <> '')
+        CONSTRAINT max_len_name CHECK (LENGTH(name) <= 256),
     parent_id BIGINT DEFAULT NULL REFERENCES public."category" (id)
 );
 
@@ -27,14 +32,19 @@ CREATE TABLE IF NOT EXISTS public."product"
     id              BIGINT                   DEFAULT NEXTVAL('product_id_seq'::regclass) NOT NULL PRIMARY KEY,
     saler_id        BIGINT                                                               NOT NULL REFERENCES public."user" (id),
     category_id     BIGINT                                                               NOT NULL REFERENCES public."category" (id),
-    title           TEXT                                                                 NOT NULL CHECK (title <> '') CHECK (LENGTH(title) <= 256),
+    title           TEXT                                                                 NOT NULL CHECK (title <> '')
+        CONSTRAINT max_len_title CHECK (LENGTH(title) <= 256),
     description     TEXT                                                                 NOT NULL CHECK (description <> '')
         CONSTRAINT max_len_description CHECK (LENGTH(description) <= 4000),
-    price           BIGINT                   DEFAULT 0                                   NOT NULL CHECK (price >= 0),
+    price           BIGINT                   DEFAULT 0                                   NOT NULL
+        CONSTRAINT not_negative_price CHECK (price >= 0),
     created_at      TIMESTAMP WITH TIME ZONE DEFAULT NOW()                               NOT NULL,
-    views           INT                      DEFAULT 0                                   NOT NULL CHECK (views >= 0),
-    available_count INT                      DEFAULT 0                                   NOT NULL CHECK (available_count >= 0),
-city            TEXT                                                                     NOT NULL CHECK (city <> '') CHECK (LENGTH(city) <= 256),
+    views           INT                      DEFAULT 0                                   NOT NULL
+        CONSTRAINT not_negative_views CHECK (views >= 0),
+    available_count INT                      DEFAULT 0                                   NOT NULL
+        CONSTRAINT max_len_available_count CHECK (available_count >= 0),
+    city            TEXT                                                                 NOT NULL CHECK (city <> '')
+        CONSTRAINT max_len_city CHECK (LENGTH(city) <= 256),
     delivery        BOOLEAN                  DEFAULT FALSE                               NOT NULL,
     safe_deal       BOOLEAN                  DEFAULT FALSE                               NOT NULL,
     is_active       BOOLEAN                  DEFAULT FALSE                               NOT NULL,
@@ -43,21 +53,23 @@ city            TEXT                                                            
 
 CREATE TABLE IF NOT EXISTS public."order"
 (
-    id          BIGINT                   DEFAULT NEXTVAL('order_id_seq'::regclass) NOT NULL PRIMARY KEY,
-    owner_id    BIGINT                                                             NOT NULL REFERENCES public."user" (id),
-    product_id  BIGINT                                                             NOT NULL REFERENCES public."product" (id),
-    count       SMALLINT                                                           NOT NULL DEFAULT 1 CHECK (count > 0),
-    status      SMALLINT                                                           NOT NULL DEFAULT 0
+    id         BIGINT                   DEFAULT NEXTVAL('order_id_seq'::regclass) NOT NULL PRIMARY KEY,
+    owner_id   BIGINT                                                             NOT NULL REFERENCES public."user" (id),
+    product_id BIGINT                                                             NOT NULL REFERENCES public."product" (id),
+    count      SMALLINT                                                           NOT NULL DEFAULT 1
+        CONSTRAINT positive_count CHECK (count > 0),
+    status     SMALLINT                                                           NOT NULL DEFAULT 0
         CONSTRAINT status_contract CHECK ( status BETWEEN 0 AND 3),
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT NOW()                             NOT NULL,
-    updated_at  TIMESTAMP WITH TIME ZONE DEFAULT NOW()                             NOT NULL,
-    closed_at   TIMESTAMP WITH TIME ZONE DEFAULT NULL
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()                             NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()                             NOT NULL,
+    closed_at  TIMESTAMP WITH TIME ZONE DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public."image"
 (
     id         BIGINT DEFAULT NEXTVAL('image_id_seq'::regclass) NOT NULL PRIMARY KEY,
-    url        TEXT                                             NOT NULL CHECK (url <> '') CHECK (LENGTH(url) <= 256),
+    url        TEXT                                             NOT NULL CHECK (url <> '')
+        CONSTRAINT max_len_url CHECK (LENGTH(url) <= 256),
     product_id BIGINT                                           NOT NULL REFERENCES public."product" (id) ON DELETE CASCADE
 );
 
