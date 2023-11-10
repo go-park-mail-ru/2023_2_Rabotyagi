fd6a3640167fb702091cc18f7bc4e8d31434df4b
Никита Демирев <nikdem00@gmail.com>
2023-11-10 15:50:46 +0300

    урлы и деплой

--- internal/pkg/server/delivery/mux/mux.go
+++ internal/pkg/server/delivery/mux/mux.go
@@ -63,24 +63,24 @@ func NewMux(ctx context.Context, configMux *ConfigMux, userStorage userusecases.
 	router.Handle("/api/v1/signin", middleware.Context(ctx, userHandler.SignInHandler))
 	router.Handle("/api/v1/logout", middleware.Context(ctx, userHandler.LogOutHandler))
 
-	router.Handle("/api/v1/profile/get/", middleware.Context(ctx, userHandler.GetUserHandler))
+	router.Handle("/api/v1/profile/get", middleware.Context(ctx, userHandler.GetUserHandler))
 	router.Handle("/api/v1/profile/update", middleware.Context(ctx, userHandler.PartiallyUpdateUserHandler))
 
 	router.Handle("/api/v1/product/add", middleware.Context(ctx, productHandler.AddProductHandler))
-	router.Handle("/api/v1/product/get/", middleware.Context(ctx, productHandler.GetProductHandler))
+	router.Handle("/api/v1/product/get", middleware.Context(ctx, productHandler.GetProductHandler))
 	router.Handle("/api/v1/product/get_list", middleware.Context(ctx, productHandler.GetProductListHandler))
 	router.Handle("/api/v1/product/get_list_of_saler",
 		middleware.Context(ctx, productHandler.GetListProductOfSalerHandler))
-	router.Handle("/api/v1/product/update/", middleware.Context(ctx, productHandler.UpdateProductHandler))
-	router.Handle("/api/v1/product/close/", middleware.Context(ctx, productHandler.CloseProductHandler))
-	router.Handle("/api/v1/product/delete/", middleware.Context(ctx, productHandler.DeleteProductHandler))
+	router.Handle("/api/v1/product/update", middleware.Context(ctx, productHandler.UpdateProductHandler))
+	router.Handle("/api/v1/product/close", middleware.Context(ctx, productHandler.CloseProductHandler))
+	router.Handle("/api/v1/product/delete", middleware.Context(ctx, productHandler.DeleteProductHandler))
 
 	router.Handle("/api/v1/order/add", middleware.Context(ctx, productHandler.AddOrderHandler))
 	router.Handle("/api/v1/order/get_basket", middleware.Context(ctx, productHandler.GetBasketHandler))
 	router.Handle("/api/v1/order/update_count", middleware.Context(ctx, productHandler.UpdateOrderCountHandler))
 	router.Handle("/api/v1/order/update_status", middleware.Context(ctx, productHandler.UpdateOrderStatusHandler))
 	router.Handle("/api/v1/order/buy_full_basket", middleware.Context(ctx, productHandler.BuyFullBasketHandler))
-	router.Handle("/api/v1/order/delete/", middleware.Context(ctx, productHandler.DeleteOrderHandler))
+	router.Handle("/api/v1/order/delete", middleware.Context(ctx, productHandler.DeleteOrderHandler))
 
 	router.Handle("/api/v1/category/get_full", middleware.Context(ctx, categoryHandler.GetFullCategories))
 
