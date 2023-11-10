aa91170a86add7a4a57a6db085f1e3e5ad7b048a
Vilinvil <ivn15072002@gmail.com>
2023-11-11 02:25:20 +0300

    [2-]-dev-VilinVil
    added check active and count in getNewProduct

--- internal/pkg/product/repository/product_storage_pg.go
+++ internal/pkg/product/repository/product_storage_pg.go
@@ -294,8 +294,10 @@ func (p *ProductStorage) GetNewProducts(ctx context.Context,
 	var slProduct []*models.ProductInFeed
 
 	err := pgx.BeginFunc(ctx, p.pool, func(tx pgx.Tx) error {
+		whereClause := fmt.Sprintf("WHERE id=%d AND is_active = true AND available_count > 0", lastProductID)
+
 		slProductInner, err := p.selectProductsInFeedWithWhereOrderLimit(ctx,
-			tx, count, squirrel.Gt{"id": lastProductID}, []string{"created_at DESC"})
+			tx, count, whereClause, []string{"created_at DESC"})
 		if err != nil {
 			return err
 		}
