359a411b4aefe05b383b7fc0167a38f59b05ae19
Vilinvil <ivn15072002@gmail.com>
2023-11-26 18:01:44 +0300

    [2-]-dev-VilinVil
    fixed documentation on GetFavouritesHandler

--- internal/pkg/product/delivery/favourite_handler.go
+++ internal/pkg/product/delivery/favourite_handler.go
@@ -28,7 +28,7 @@ type IFavouriteService interface {
 //	@Failure    405  {string} string
 //	@Failure    500  {string} string
 //	@Failure    222  {object} delivery.ErrorResponse "Error"
-//	@Router      /profile/profile/favourites [get]
+//	@Router      /profile/favourites [get]
 func (p *ProductHandler) GetFavouritesHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, `Method not allowed`, http.StatusMethodNotAllowed)
