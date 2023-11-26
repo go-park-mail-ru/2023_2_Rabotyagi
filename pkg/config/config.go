0739f27feb965be58093323d7dfdbc6977af525b
Vilinvil <ivn15072002@gmail.com>
2023-11-26 14:13:35 +0300

    [3-5]-grpc-fs-VilinVil
    changed config const declaration

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,28 +3,36 @@ package config
 import "os"
 
 const (
+	// Main env
+
 	StandardDevelopmentMode    = "development"
 	StandardProductionMode     = "production"
 	StandardAllowOrigin        = "localhost:3000"
 	StandardSchema             = "http://"
 	StandardPort               = "8080"
-	StandardURLDataBase        = "postgres://postgres:postgres@localhost:5432/youla?sslmode=disable"
-	StandardPathToRoot         = "."
-	StandardFileServiceDir     = "./static/img"
 	StandardOutputLogPath      = "stdout /var/log/backend/logs.json"
 	StandardErrorOutputLogPath = "stderr /var/log/backend/err_logs.json"
+	StandardURLDataBase        = "postgres://postgres:postgres@localhost:5432/youla?sslmode=disable"
 	StandardSecret             = ""
 
 	EnvEnvironmentMode    = "ENVIRONMENT"
 	EnvAllowOrigin        = "ALLOW_ORIGIN"
 	EnvSchema             = "SCHEMA"
 	EnvPortBackend        = "PORT_BACKEND"
-	EnvURLDataBase        = "URL_DATA_BASE"
-	EnvPathToRoot         = "PATH_TO_ROOT"
-	EnvFileServiceDir     = "FILE_SERVICE_DIR"
 	EnvOutputLogPath      = "OUTPUT_LOG_PATH"
 	EnvErrorOutputLogPath = "ERROR_OUTPUT_LOG_PATH"
+	EnvURLDataBase        = "URL_DATA_BASE"
 	EnvStandardSecret     = "JWT_SECRET"
+
+	// File service env
+
+	StandardPathToRoot         = "."
+	StandardFileServiceDir     = "./static/img"
+	StandardAddressFileService = "127.0.0.1:8081"
+
+	EnvPathToRoot         = "PATH_TO_ROOT"
+	EnvFileServiceDir     = "FILE_SERVICE_DIR"
+	EnvAddressFileService = "ADDRESS_FILE_SERVICE"
 )
 
 func GetEnvStr(name string, defaultValue string) string {
