176a1a949f5e1d7117a12c24c57f4ac8bb8c874e
SanExpett <alexkrasnoperov09@gmail.com>
2023-11-27 21:07:38 +0300

    [] fix SanExpett

--- internal/pkg/product/repository/product_storage_pg.go
+++ internal/pkg/product/repository/product_storage_pg.go
@@ -746,7 +746,7 @@ func (p *ProductStorage) UpdateAllViews(ctx context.Context) error {
 func (p *ProductStorage) searchProduct(ctx context.Context, tx pgx.Tx, searchInput string) ([]string, error) {
 	SQLSearchProduct := `SELECT title
 							FROM product
-							WHERE (to_tsvector(title) @@ to_tsquery(replace($1 || ':*', ' ', ' | '))
+							WHERE to_tsvector(title) @@ to_tsquery(replace($1 || ':*', ' ', ' | '))
 							   AND ts_rank(to_tsvector(title), to_tsquery(replace($1 || ':*', ' ', ' | '))) > 0
 							   AND is_active = true
 							ORDER BY ts_rank(to_tsvector(title), to_tsquery(replace($1 || ':*', ' ', ' | '))) DESC
