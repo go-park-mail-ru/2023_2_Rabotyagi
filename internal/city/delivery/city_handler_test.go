6815636258016dcd82b07fca6279101220f5b08f
Vilinvil <ivn15072002@gmail.com>
2023-11-29 05:17:22 +0300

    [3-6]-tests-VilinVil
    added SearchCity in city_handler

--- internal/city/delivery/city_handler_test.go
+++ internal/city/delivery/city_handler_test.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"testing"
 
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/city/delivery"
@@ -13,7 +12,7 @@ import (
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/models"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/my_logger"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/responses/statuses"
-
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/utils"
 	"go.uber.org/mock/gomock"
 )
 
@@ -34,10 +33,50 @@ func TestGetFullCities(t *testing.T) {
 			name: "test basic work",
 			behavior: func(m *mocks.MockICityService) {
 				m.EXPECT().GetFullCities(gomock.Any()).Return([]*models.City{
-					{ID: 1, Name: "Moscow"}, {ID: 2, Name: "Voronezh"}}, nil)
+					{ID: 1, Name: "Moscow"}, {ID: 2, Name: "Voronezh"},
+				}, nil)
 			},
-			expectedResponse: delivery.CityListResponse{Status: statuses.StatusResponseSuccessful,
-				Body: []*models.City{{ID: 1, Name: "Moscow"}, {ID: 2, Name: "Voronezh"}}},
+			expectedResponse: delivery.CityListResponse{
+				Status: statuses.StatusResponseSuccessful,
+				Body: []*models.City{
+					{ID: 1, Name: "Moscow"}, {ID: 2, Name: "Voronezh"},
+				},
+			},
+		},
+		{
+			name: "test empty work",
+			behavior: func(m *mocks.MockICityService) {
+				m.EXPECT().GetFullCities(gomock.Any()).Return([]*models.City{}, nil)
+			},
+			expectedResponse: delivery.CityListResponse{
+				Status: statuses.StatusResponseSuccessful,
+				Body:   []*models.City{},
+			},
+		},
+		{
+			name: "test repeated names a lot",
+			behavior: func(m *mocks.MockICityService) {
+				m.EXPECT().GetFullCities(gomock.Any()).Return(
+					[]*models.City{
+						{ID: 1, Name: "Moscow"},
+						{ID: 1, Name: "Moscow"},
+						{ID: 1, Name: "Moscow"},
+						{ID: 1, Name: "Moscow"},
+						{ID: 1, Name: "Moscow"},
+						{ID: 1, Name: "Moscow"},
+					}, nil)
+			},
+			expectedResponse: delivery.CityListResponse{
+				Status: statuses.StatusResponseSuccessful,
+				Body: []*models.City{
+					{ID: 1, Name: "Moscow"},
+					{ID: 1, Name: "Moscow"},
+					{ID: 1, Name: "Moscow"},
+					{ID: 1, Name: "Moscow"},
+					{ID: 1, Name: "Moscow"},
+					{ID: 1, Name: "Moscow"},
+				},
+			},
 		},
 	}
 
@@ -80,9 +119,118 @@ func TestGetFullCities(t *testing.T) {
 				t.Fatalf("Failed to Unmarshal(receivedResponse): %v", err)
 			}
 
-			if !reflect.DeepEqual(testCase.expectedResponse, resultResponse) {
-				t.Errorf("Wrong Response: got %+v, expected %+v",
-					resultResponse, testCase.expectedResponse)
+			if resultResponse.Status != testCase.expectedResponse.Status {
+				t.Errorf("Wrong Response: got status %+v, expected status%+v", resultResponse.Status,
+					testCase.expectedResponse.Status)
+			}
+
+			err = utils.EqualTest(resultResponse, testCase.expectedResponse)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+//nolint:funlen
+func TestSearchCityHandler(t *testing.T) {
+	t.Parallel()
+
+	_ = my_logger.NewNop()
+
+	type TestCase struct {
+		name             string
+		query            string
+		behavior         func(m *mocks.MockICityService)
+		expectedResponse delivery.CityListResponse
+	}
+
+	testCases := [...]TestCase{
+		{
+			name:  "test basic work",
+			query: "Москва",
+			behavior: func(m *mocks.MockICityService) {
+				m.EXPECT().SearchCity(gomock.Any(), "Москва").Return([]*models.City{
+					{ID: 1, Name: "Moscow"},
+				}, nil)
+			},
+			expectedResponse: delivery.CityListResponse{
+				Status: statuses.StatusResponseSuccessful,
+				Body: []*models.City{
+					{ID: 1, Name: "Moscow"},
+				},
+			},
+		},
+		{
+			name:  "test empty query",
+			query: "",
+			behavior: func(m *mocks.MockICityService) {
+				m.EXPECT().SearchCity(gomock.Any(), "").Return([]*models.City{}, nil)
+			},
+			expectedResponse: delivery.CityListResponse{
+				Status: statuses.StatusResponseSuccessful,
+				Body:   []*models.City{},
+			},
+		},
+		{
+			name:  "test special symbols query",
+			query: "Москва &&& Вологда",
+			behavior: func(m *mocks.MockICityService) {
+				m.EXPECT().SearchCity(gomock.Any(), "Москва &&& Вологда").Return([]*models.City{
+					{ID: 1, Name: "Вологда"}, {ID: 1, Name: "Вологда"},
+				}, nil)
+			},
+			expectedResponse: delivery.CityListResponse{
+				Status: statuses.StatusResponseSuccessful,
+				Body: []*models.City{
+					{ID: 1, Name: "Вологда"}, {ID: 1, Name: "Вологда"},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockServ := mocks.NewMockICityService(ctrl)
+
+			testCase.behavior(mockServ)
+
+			cityHandler, err := delivery.NewCityHandler(mockServ)
+			if err != nil {
+				t.Fatalf("UnExpected err=%+v\n", err)
+			}
+
+			req := httptest.NewRequest(http.MethodGet, "/api/v1/city/search", nil)
+			utils.AddQueryParamsToRequest(req, map[string]string{"searched": testCase.query})
+			w := httptest.NewRecorder()
+
+			cityHandler.SearchCityHandler(w, req)
+
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			receivedResponse, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Failed to ReadAll resp.Body: %v", err)
+			}
+
+			var resultResponse delivery.CityListResponse
+
+			err = json.Unmarshal(receivedResponse, &resultResponse)
+			if err != nil {
+				t.Fatalf("Failed to Unmarshal(receivedResponse): %v", err)
+			}
+
+			err = utils.EqualTest(resultResponse, testCase.expectedResponse)
+			if err != nil {
+				t.Fatal(err)
 			}
 		})
 	}
