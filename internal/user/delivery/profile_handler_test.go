480dfc97c260da37c2940893e9faeac7360ead77
Vilinvil <ivn15072002@gmail.com>
2023-12-01 23:20:02 +0300

    [3-6]-tests-VilinVil
    added TestGetUser

--- internal/user/delivery/profile_handler_test.go
+++ internal/user/delivery/profile_handler_test.go
@@ -3,13 +3,6 @@ package delivery_test
 import (
 	"database/sql"
 	"fmt"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/auth"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/models"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/my_logger"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/myerrors"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/responses"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/responses/statuses"
-	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/utils/test"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -18,7 +11,14 @@ import (
 
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/user/delivery"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/user/mocks"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/auth"
 	mocksauth "github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/auth/mocks"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/models"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/my_logger"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/myerrors"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/responses"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/responses/statuses"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/utils/test"
 
 	"go.uber.org/mock/gomock"
 )
@@ -39,6 +39,87 @@ func NewProfileHandler(ctrl *gomock.Controller,
 	}
 
 	return profileHandler, nil
+}
+
+//nolint:funlen
+func TestGetUser(t *testing.T) {
+	t.Parallel()
+
+	_ = my_logger.NewNop()
+
+	type TestCase struct {
+		name                string
+		behaviorUserService func(m *mocks.MockIUserService)
+		request             *http.Request
+		expectedResponse    any
+	}
+
+	testCases := [...]TestCase{
+		{
+			name:    "test basic work",
+			request: httptest.NewRequest(http.MethodGet, "/api/v1/profile/get?id=1", nil),
+			behaviorUserService: func(m *mocks.MockIUserService) {
+				m.EXPECT().GetUserWithoutPasswordByID(gomock.Any(), test.UserID).Return(
+					&models.UserWithoutPassword{ //nolint:exhaustruct
+						ID:    test.UserID,
+						Email: "new_email@mail.ru", Name: sql.NullString{Valid: true, String: "test_name"},
+					}, nil)
+			},
+			expectedResponse: delivery.NewProfileResponse(&models.UserWithoutPassword{ //nolint:exhaustruct
+				ID:    test.UserID,
+				Email: "new_email@mail.ru", Name: sql.NullString{Valid: true, String: "test_name"},
+			}),
+		},
+		{
+			name:    "test internal error",
+			request: httptest.NewRequest(http.MethodGet, "/api/v1/profile/get?id=1", nil),
+			behaviorUserService: func(m *mocks.MockIUserService) {
+				m.EXPECT().GetUserWithoutPasswordByID(gomock.Any(), test.UserID).Return(nil,
+					myerrors.NewErrorInternal("Test error"))
+			},
+			expectedResponse: responses.NewErrResponse(statuses.StatusInternalServer, responses.ErrInternalServer),
+		},
+		{
+			name:                "test method not allowed",
+			request:             httptest.NewRequest(http.MethodDelete, "/api/v1/profile/get?id=1", nil),
+			behaviorUserService: func(m *mocks.MockIUserService) {},
+			expectedResponse: `Method not allowed
+`,
+		},
+		{
+			name:                "test error bad format",
+			request:             httptest.NewRequest(http.MethodGet, "/api/v1/profile/get?id=bad_format", nil),
+			behaviorUserService: func(m *mocks.MockIUserService) {},
+			expectedResponse: responses.NewErrResponse(statuses.StatusBadFormatRequest,
+				"Получили некорректный числовой параметр. Он должен быть целым id=bad_format"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			profileHandler, err := NewProfileHandler(ctrl, testCase.behaviorUserService,
+				func(m *mocksauth.MockSessionMangerClient) {})
+			if err != nil {
+				t.Fatalf("Failed create profileHandler %s", err.Error())
+			}
+
+			w := httptest.NewRecorder()
+
+			profileHandler.GetUserHandler(w, testCase.request)
+
+			err = test.CompareHTTPTestResult(w, testCase.expectedResponse)
+			if err != nil {
+				t.Fatalf("Failed CompareHTTPTestResult %+v", err)
+			}
+		})
+	}
 }
 
 //nolint:funlen
@@ -91,7 +172,7 @@ func TestPartiallyUpdateUser(t *testing.T) {
 				responses.ErrCookieNotPresented.Status(), responses.ErrCookieNotPresented.Error()),
 		},
 		{
-			name: "test internal in SessionManagerClient",
+			name: "test internal error in SessionManagerClient",
 			request: func() *http.Request {
 				req := httptest.NewRequest(http.MethodPatch, "/api/v1/profile/update", nil)
 				req.AddCookie(&test.Cookie) //nolint:exhaustruct
@@ -114,7 +195,7 @@ func TestPartiallyUpdateUser(t *testing.T) {
 `,
 		},
 		{
-			name: "test error put internal error",
+			name: "test put internal error",
 			request: func() *http.Request {
 				req := httptest.NewRequest(http.MethodPut, "/api/v1/profile/update", strings.NewReader(
 					`{"email":"new_email@mail.ru, "name":"test"}`))
