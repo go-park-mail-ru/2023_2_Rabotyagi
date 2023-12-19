34445ee530f379877cfda6464caa9773432f5544
alexey <alexkrasnoperov09@gmail.com>
2023-12-19 19:01:49 +0300

    [4-3] fix feed SanExpett

--- internal/product/repository/order_by_clause.go
+++ internal/product/repository/order_by_clause.go
@@ -6,9 +6,9 @@ func OrderByClauseForProductList(premiumCoefficient, nonPremiumCoefficient,
 	soldByUserCoefficient, viewsCoefficient uint16) string { //nolint: gofumpt
 	return fmt.Sprintf(`CASE
 		WHEN premium = true THEN ((%d * views + %d * (SELECT COUNT(*) FROM product p2
-		WHERE p2.saler_id = product.saler_id)) * %d)
+		WHERE p2.saler_id = product.saler_id)) * %d) 
 		ELSE ((%d * views + %d * (SELECT COUNT(*) FROM product p2
-		WHERE p2.saler_id = product.saler_id)) * %d)
-		END`, premiumCoefficient, soldByUserCoefficient, premiumCoefficient,
+		WHERE p2.saler_id = product.saler_id)) * %d) 
+		END DESC`, premiumCoefficient, soldByUserCoefficient, premiumCoefficient,
 		viewsCoefficient, soldByUserCoefficient, nonPremiumCoefficient)
 }
