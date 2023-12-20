b6af5f70d72523f61a7d3f2ee38363448cee56d7
Vilinvil <ivn15072002@gmail.com>
2023-12-20 12:09:32 +0300

    [3-]-dev-VilinVil
    added test-actions.sh, added test and golangci jobs in dev-build.yaml

---
+++ scripts/test-actions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+COVERAGE=$(go test --race -coverpkg=./... -coverprofile=cover.out ./... &&
+  cat cover.out | grep -v "mocks" | grep -v "easyjson" | grep -v ".pb" > pure_cover.out &&
+  go tool cover --func pure_cover.out)
+COVERAGE_PURE=$(echo $COVERAGE | grep -oP '\(statements\)(\s+)(\d+\.\d+)' | grep -oP '(\d+\.\d+)')
+
+rm cover.out;
+rm pure_cover.out;
+
+if [ $(echo "${COVERAGE_PURE} < 6.00" | bc) -eq 1 ]
+then
+  echo "Coverage is $COVERAGE_PURE less than 60%";
+  exit 1;
+else
+  echo "Coverage is $COVERAGE_PURE more than 60%";
+fi
