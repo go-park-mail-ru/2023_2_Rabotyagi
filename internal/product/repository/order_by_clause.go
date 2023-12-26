591e95509bccc73be8d48c3d15cbcd86e0e893fe
Vilinvil <ivn15072002@gmail.com>
2023-12-27 00:27:03 +0300

    [4-]-dev-VilinVil
    fixed bug in OrderByClauseForProductList where not exist premium column

--- internal/product/repository/order_by_clause.go
+++ internal/product/repository/order_by_clause.go
@@ -1,14 +1,18 @@
 package repository
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/responses/statuses"
+)
 
 func OrderByClauseForProductList(premiumCoefficient, nonPremiumCoefficient,
 	soldByUserCoefficient, viewsCoefficient uint16) string { //nolint: gofumpt
 	return fmt.Sprintf(`CASE
-		WHEN premium = true THEN ((%d * views + %d * (SELECT COUNT(*) FROM product p2
+		WHEN premium_status = %d THEN ((%d * views + %d * (SELECT COUNT(*) FROM product p2
 		WHERE p2.saler_id = product.saler_id)) * %d) 
 		ELSE ((%d * views + %d * (SELECT COUNT(*) FROM product p2
 		WHERE p2.saler_id = product.saler_id)) * %d) 
-		END DESC`, premiumCoefficient, soldByUserCoefficient, premiumCoefficient,
+		END DESC`, statuses.IntStatusPremiumSucceeded, premiumCoefficient, soldByUserCoefficient, premiumCoefficient,
 		viewsCoefficient, soldByUserCoefficient, nonPremiumCoefficient)
 }
