e742a74a003d8f3e0a801691297262a39364a554
alexey <alexkrasnoperov09@gmail.com>
2023-12-27 19:13:42 +0300

    [4-0] comment tests SanExpett

--- internal/product/repository/comment_storage_test.go
+++ internal/product/repository/comment_storage_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/internal/product/repository"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/models"
 	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/mylogger"
+	"github.com/go-park-mail-ru/2023_2_Rabotyagi/pkg/utils"
 	"github.com/pashagolub/pgxmock/v3"
 )
 
@@ -21,6 +22,7 @@ func TestAddComment(t *testing.T) {
 		name                   string
 		behaviorProductStorage func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface)
 		preComment             *models.PreComment
+		expectedResponse       uint64
 	}
 
 	testCases := [...]TestCase{
@@ -40,7 +42,8 @@ func TestAddComment(t *testing.T) {
 				mockPool.ExpectCommit()
 				mockPool.ExpectRollback()
 			},
-			preComment: &models.PreComment{SenderID: uint64(1), RecipientID: uint64(2), Rating: uint8(5), Text: "good"},
+			preComment:       &models.PreComment{SenderID: uint64(1), RecipientID: uint64(2), Rating: uint8(5), Text: "good"},
+			expectedResponse: uint64(1),
 		},
 		{
 			name: "test 0 rows affected",
@@ -58,7 +61,8 @@ func TestAddComment(t *testing.T) {
 				mockPool.ExpectCommit()
 				mockPool.ExpectRollback()
 			},
-			preComment: &models.PreComment{SenderID: uint64(1), RecipientID: uint64(2), Rating: 5, Text: ""},
+			preComment:       &models.PreComment{SenderID: uint64(1), RecipientID: uint64(2), Rating: 5, Text: ""},
+			expectedResponse: uint64(1),
 		},
 	}
 
@@ -82,7 +86,7 @@ func TestAddComment(t *testing.T) {
 
 			testCase.behaviorProductStorage(catStorage, mockPool)
 
-			_, err = catStorage.AddComment(ctx, testCase.preComment)
+			response, err := catStorage.AddComment(ctx, testCase.preComment)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -90,31 +94,32 @@ func TestAddComment(t *testing.T) {
 			if err := mockPool.ExpectationsWereMet(); err != nil {
 				t.Errorf("there were unfulfilled expectations: %s", err)
 			}
+
+			err = utils.EqualTest(response, testCase.expectedResponse)
+			if err != nil {
+				t.Fatal(err)
+			}
 		})
 	}
 }
 
-func TestDeleteComment(t *testing.T) { //nolint:dupl
+func TestDeleteComment(t *testing.T) {
 	t.Parallel()
 
 	_ = mylogger.NewNop()
 
-	mockPool, err := pgxmock.NewPool()
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-
 	type TestCase struct {
 		name                   string
-		behaviorProductStorage func(m *repository.ProductStorage)
+		behaviorProductStorage func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface)
 		userID                 uint64
 		commentID              uint64
+		expectedError          error
 	}
 
 	testCases := [...]TestCase{
 		{
 			name: "test basic work",
-			behaviorProductStorage: func(m *repository.ProductStorage) {
+			behaviorProductStorage: func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface) {
 				mockPool.ExpectBegin()
 
 				mockPool.ExpectExec(`DELETE FROM public."comment"`).WithArgs(uint64(1), uint64(1)).
@@ -123,8 +128,24 @@ func TestDeleteComment(t *testing.T) { //nolint:dupl
 				mockPool.ExpectCommit()
 				mockPool.ExpectRollback()
 			},
-			userID:    1,
-			commentID: 1,
+			userID:        1,
+			commentID:     1,
+			expectedError: nil,
+		},
+		{
+			name: "test no affected rows",
+			behaviorProductStorage: func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface) {
+				mockPool.ExpectBegin()
+
+				mockPool.ExpectExec(`DELETE FROM public."comment"`).WithArgs(uint64(1), uint64(1)).
+					WillReturnResult(pgxmock.NewResult("DELETE", 0))
+
+				mockPool.ExpectRollback()
+				mockPool.ExpectRollback()
+			},
+			userID:        1,
+			commentID:     1,
+			expectedError: repository.ErrNoAffectedCommentRows,
 		},
 	}
 
@@ -136,20 +157,30 @@ func TestDeleteComment(t *testing.T) { //nolint:dupl
 
 			ctx := context.Background()
 
+			mockPool, err := pgxmock.NewPool()
+			if err != nil {
+				t.Fatalf("%v", err)
+			}
+
 			commentStorage, err := repository.NewProductStorage(mockPool)
 			if err != nil {
 				t.Fatalf("%v", err)
 			}
 
-			testCase.behaviorProductStorage(commentStorage)
+			testCase.behaviorProductStorage(commentStorage, mockPool)
 
-			err = commentStorage.DeleteComment(ctx, testCase.commentID, testCase.userID)
+			errActual := commentStorage.DeleteComment(ctx, testCase.commentID, testCase.userID)
 			if err != nil {
 				t.Fatal(err)
 			}
 
 			if err := mockPool.ExpectationsWereMet(); err != nil {
 				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+
+			err = utils.EqualError(errActual, testCase.expectedError)
+			if err != nil {
+				t.Fatal(err)
 			}
 		})
 	}
@@ -167,6 +198,7 @@ func TestGetCommentList(t *testing.T) {
 		senderID               uint64
 		offset                 uint64
 		count                  uint64
+		expectedResponse       []*models.CommentInFeed
 	}
 
 	testCases := [...]TestCase{
@@ -195,6 +227,35 @@ FROM public."comment" c`).WithArgs(uint64(1), uint64(2), uint64(1), uint64(1)).
 			senderID: 2,
 			offset:   1,
 			count:    1,
+			expectedResponse: []*models.CommentInFeed{
+				{
+					ID: 1, SenderID: 2, SenderName: "Ivan", Avatar: sql.NullString{Valid: false, String: ""},
+					Text: "good", Rating: 5, CreatedAt: time.Time{},
+				},
+			},
+		},
+		{
+			name: "test empty",
+			behaviorProductStorage: func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface) {
+				mockPool.ExpectBegin()
+
+				mockPool.ExpectQuery(`SELECT c.id AS comment_id, c.sender_id, 
+       CASE WHEN u.name IS NOT NULL THEN u.name ELSE u.email END,
+       u.avatar,
+       c.text,
+       c.rating,
+       c.created_at
+FROM public."comment" c`).WithArgs(uint64(1), uint64(2), uint64(1), uint64(1)).
+					WillReturnRows(pgxmock.NewRows([]string{}))
+
+				mockPool.ExpectCommit()
+				mockPool.ExpectRollback()
+			},
+			resID:            1,
+			senderID:         2,
+			offset:           1,
+			count:            1,
+			expectedResponse: nil,
 		},
 		{
 			name: "test more rows",
@@ -223,6 +284,20 @@ FROM public."comment" c`).WithArgs(uint64(1), uint64(2), uint64(1), uint64(1)).
 			senderID: 2,
 			offset:   1,
 			count:    1,
+			expectedResponse: []*models.CommentInFeed{
+				{
+					ID: 1, SenderID: 2, SenderName: "Ivan", Avatar: sql.NullString{Valid: false, String: ""},
+					Text: "good", Rating: 5, CreatedAt: time.Time{},
+				},
+				{
+					ID: 2, SenderID: 3, SenderName: "Petr", Avatar: sql.NullString{Valid: false, String: ""},
+					Text: "bad", Rating: 2, CreatedAt: time.Time{},
+				},
+				{
+					ID: 3, SenderID: 4, SenderName: "Mark", Avatar: sql.NullString{Valid: false, String: ""},
+					Text: "not bad", Rating: 3, CreatedAt: time.Time{},
+				},
+			},
 		},
 	}
 
@@ -246,13 +321,19 @@ FROM public."comment" c`).WithArgs(uint64(1), uint64(2), uint64(1), uint64(1)).
 
 			testCase.behaviorProductStorage(commentStorage, mockPool)
 
-			_, err = commentStorage.GetCommentList(ctx, testCase.offset, testCase.count, testCase.resID, testCase.senderID)
+			response, err := commentStorage.GetCommentList(ctx, testCase.offset, testCase.count,
+				testCase.resID, testCase.senderID)
 			if err != nil {
 				t.Fatal(err)
 			}
 
 			if err := mockPool.ExpectationsWereMet(); err != nil {
 				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+
+			err = utils.EqualTest(response, testCase.expectedResponse)
+			if err != nil {
+				t.Fatal(err)
 			}
 		})
 	}
@@ -263,23 +344,19 @@ func TestCommentUpdate(t *testing.T) {
 
 	_ = mylogger.NewNop()
 
-	mockPool, err := pgxmock.NewPool()
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-
 	type TestCase struct {
 		name                   string
-		behaviorProductStorage func(m *repository.ProductStorage)
+		behaviorProductStorage func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface)
 		userID                 uint64
 		commentID              uint64
 		updateFields           map[string]interface{}
+		expectedError          error
 	}
 
 	testCases := [...]TestCase{
 		{
 			name: "test basic work",
-			behaviorProductStorage: func(m *repository.ProductStorage) {
+			behaviorProductStorage: func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface) {
 				mockPool.ExpectBegin()
 
 				mockPool.ExpectExec(`UPDATE public."comment"`).WithArgs(uint8(3), "not bad", uint64(1), uint64(1)).
@@ -288,9 +365,26 @@ func TestCommentUpdate(t *testing.T) {
 				mockPool.ExpectCommit()
 				mockPool.ExpectRollback()
 			},
-			userID:       1,
-			commentID:    1,
-			updateFields: map[string]interface{}{"rating": uint8(3), "text": "not bad"},
+			userID:        1,
+			commentID:     1,
+			updateFields:  map[string]interface{}{"rating": uint8(3), "text": "not bad"},
+			expectedError: nil,
+		},
+		{
+			name: "test no rows",
+			behaviorProductStorage: func(m *repository.ProductStorage, mockPool pgxmock.PgxPoolIface) {
+				mockPool.ExpectBegin()
+
+				mockPool.ExpectExec(`UPDATE public."comment"`).WithArgs(uint8(3), "not bad", uint64(1), uint64(1)).
+					WillReturnResult(pgxmock.NewResult("INSERT", 0))
+
+				mockPool.ExpectRollback()
+				mockPool.ExpectRollback()
+			},
+			userID:        1,
+			commentID:     1,
+			updateFields:  map[string]interface{}{"rating": uint8(3), "text": "not bad"},
+			expectedError: repository.ErrNoAffectedCommentRows,
 		},
 	}
 
@@ -302,20 +396,30 @@ func TestCommentUpdate(t *testing.T) {
 
 			ctx := context.Background()
 
+			mockPool, err := pgxmock.NewPool()
+			if err != nil {
+				t.Fatalf("%v", err)
+			}
+
 			commentStorage, err := repository.NewProductStorage(mockPool)
 			if err != nil {
 				t.Fatalf("%v", err)
 			}
 
-			testCase.behaviorProductStorage(commentStorage)
+			testCase.behaviorProductStorage(commentStorage, mockPool)
 
-			err = commentStorage.UpdateComment(ctx, testCase.userID, testCase.commentID, testCase.updateFields)
+			errActual := commentStorage.UpdateComment(ctx, testCase.userID, testCase.commentID, testCase.updateFields)
 			if err != nil {
 				t.Fatal(err)
 			}
 
 			if err := mockPool.ExpectationsWereMet(); err != nil {
 				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+
+			err = utils.EqualError(errActual, testCase.expectedError)
+			if err != nil {
+				t.Fatal(err)
 			}
 		})
 	}
